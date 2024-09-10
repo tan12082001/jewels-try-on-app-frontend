5fb2df9d1fd4a8842b923d40b49e688f4dc0cb3b
Tanmayi Manku <tanmayitanum@gmail.com>
2024-09-11 01:28:53 +0530

    update the routes basing on user status

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ListPage from '../pages/ListPage';
 import BookingPage from '../pages/BookingPage';
 import LandingPage from '../pages/LandingPage';
@@ -8,25 +9,47 @@ import OrdersPage from '../pages/OrdersPage';
 import FavouritesPage from '../pages/FavouritesPage';
 import DetailsPage from '../pages/DetailsPage';
 import NavigationBar from './NavigationBar';
+import SigninPage from '../pages/SignInPage';
+import MyProfilePage from '../pages/ProfilePage';
+import CommonNavigation from './CommonNavigation';
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <>
-      <NavigationBar />
-      <div className="app-main-content-outer">
-        <Routes>
-          <Route path="/signin" element={<LandingPage />} />
-          <Route path="/" element={<ListPage />} />
-          <Route path="/details" element={<DetailsPage />} />
-          <Route path="/bookTryOn" element={<BookingPage />} />
-          <Route path="/myFavourites" element={<FavouritesPage />} />
-          <Route path="/myOrders" element={<OrdersPage />} />
-          <Route path="/myBookings" element={<MyBookingsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </div>
-    </>
-  </BrowserRouter>
-);
+const AppRouter = () => {
+  const user = useSelector((state) => state.userAuth.user);
+
+  return (
+    <BrowserRouter>
+      <>
+        {user ? (
+          <>
+            <CommonNavigation />
+            <NavigationBar />
+            <MyProfilePage />
+            <div className="app-main-content-outer">
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/signin" element={<SigninPage />} />
+                <Route path="/home" element={<ListPage />} />
+                <Route path="/details" element={<DetailsPage />} />
+                <Route path="/bookTryOn" element={<BookingPage />} />
+                <Route path="/myFavourites" element={<FavouritesPage />} />
+                <Route path="/myOrders" element={<OrdersPage />} />
+                <Route path="/myBookings" element={<MyBookingsPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/myprofile" element={<MyProfilePage />} />
+              </Routes>
+            </div>
+          </>
+        ) : (
+          <>
+            <CommonNavigation />
+            <Routes>
+              <Route path="/" element={<ListPage />} />
+            </Routes>
+          </>
+        )}
+      </>
+    </BrowserRouter>
+  );
+};
 
 export default AppRouter;
