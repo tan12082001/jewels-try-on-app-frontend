936ce4f7248e3456971ed9655e854d9ed104a895
Tanmayi Manku <tanmayitanum@gmail.com>
2024-09-16 23:27:55 +0530

    Apply vertical text fade animation

--- src/components/DisplayProducts.js
+++ src/components/DisplayProducts.js
@@ -1,10 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 import { fetchProducts } from '../redux/api';
 
 const DisplayProducts = () => {
   const dispatch = useDispatch();
-
+  const [currentText, setCurrentText] = useState('Earrings');
+  const texts = [
+    'Earrings',
+    'Chains',
+    'Anklets',
+    'Bangels',
+    'Bracelets',
+    'Rings',
+    'Leather Bags',
+    'Key Chains',
+  ];
   const products = useSelector((state) => state.products.products);
   const mode = useSelector((state) => state.userAuth.mode);
   const repeatedproducts = [...products, ...products, ...products];
@@ -15,9 +26,33 @@ const DisplayProducts = () => {
     }
   }, [dispatch, products.length]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentText((prevText) => {
+        const currentIndex = texts.indexOf(prevText);
+        const nextIndex = (currentIndex + 1) % texts.length;
+        return texts[nextIndex];
+      });
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="listpage-inner">
-      <h2 className={`list-page-h2-heading ${mode ? 'dark-title' : 'light-title'}`}>Now Trending</h2>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h2 className={`list-page-h2-heading ${mode ? 'dark-title' : 'light-title'}`}>
+          Now Trending
+          <span id="changing-product-titles">
+            {' '}
+            {currentText}
+          </span>
+        </h2>
+      </motion.div>
       <ul className="tiles-div-outer">
         {repeatedproducts.map((product) => (
           <div className={`each-product-tile ${mode ? 'white-tile' : 'light-tile'}`} key={product.id}>
